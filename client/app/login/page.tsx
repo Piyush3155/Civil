be46ed84986b14ba3aeb27d23f14621c0c581d1f
Piyush Gurav <piyushgurav176@gmail.com>
2025-11-19 19:32:45 +0530

    UI initialize

---
+++ client/app/login/page.tsx
@@ -0,0 +1,40 @@
+import { GalleryVerticalEnd } from "lucide-react"
+import Image from "next/image"
+
+import { LoginForm } from "../../components/login-form"
+import { ModeToggle } from "@/components/theme"
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+           CIVIL DESK
+          </a>
+          <div className="flex flex-1 items-center justify-end">
+            <ModeToggle />
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+  <div className="relative hidden bg-muted lg:block h-full">
+        <Image
+          src="/civil.png"
+          alt="Image"
+          fill
+          className="object-cover dark:brightness-[0.2] dark:grayscale"
+          priority
+        />
+      </div>
+    </div>
+  
+  )
+}
